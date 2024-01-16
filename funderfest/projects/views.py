210e9075a96c35153c6b6677f7bf8c3e8397da8d
caoimhejyoti <caoimhejyoti@gmail.com>
2024-01-16 20:02:25 +0800

    adding permission classes to festival views

--- funderfest/projects/views.py
+++ funderfest/projects/views.py
@@ -3,27 +3,29 @@ from rest_framework.response import Response
 from .models import Festival, Ticket, Pledge
 from .serializers import FestivalSerializer, FestivalDetailSerializer, TicketSerializer, PledgeSerializer
 from django.http import Http404
-from rest_framework import status
+from rest_framework import status, permissions
 
 class FestivalList(APIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    
     def get(self, request):
         festivals = Festival.objects.all()
         serializer = FestivalSerializer(festivals, many=True)
         return Response(serializer.data)
 
     def post(self, request):
-            serializer = FestivalSerializer(data=request.data)
-            if serializer.is_valid():
-                serializer.save(owner=request.user)
-                return Response(
-                    serializer.data,
-                    status=status.HTTP_201_CREATED
-                )
+        serializer = FestivalSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
             return Response(
-                serializer.errors,
-                status=status.HTTP_400_BAD_REQUEST
+                serializer.data,
+                status=status.HTTP_201_CREATED
             )
-    
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
 class FestivalDetail(APIView):
     def get_object(self, pk):
         try:
