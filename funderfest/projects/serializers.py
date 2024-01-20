76095e73c7aeb768eb4374cc7e781aa9ab5b4d94
caoimhejyoti <caoimhejyoti@gmail.com>
2024-01-20 09:37:21 +0800

    ticket and pledge detail serializer

--- funderfest/projects/serializers.py
+++ funderfest/projects/serializers.py
@@ -20,6 +20,23 @@ class TicketSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ticket
         fields = "__all__"
+
+class TicketDetailSerializer(serializers.ModelSerializer):
+    # festivals = FestivalSerializer(many=True, read_only=True)
+    pledges = PledgeSerializer(many=True, read_only=True)
+    
+    def update(self, instance, validated_data):
+        instance.ticket_name = validated_data.get('ticket_name', instance.ticket_name)
+        instance.cost = validated_data.get('cost',instance.cost)
+        instance.features = validated_data.get('features', instance.features)
+        instance.festival = validated_data.get('festival', instance.festival)
+        instance.save()
+        return instance
+    
+    class Meta:
+        model = Ticket
+        fields = "__all__"
+
 class FestivalSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.id')
     class Meta:
@@ -42,9 +59,26 @@ class FestivalDetailSerializer(serializers.ModelSerializer):
         instance.save()
         return instance
 
-
-
     class Meta:
         model = Festival
         fields = "__all__"
     
+
+
+class PledgeDetailSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.id')
+    tickets = TicketSerializer(many=True, read_only=True)
+    festivals = FestivalSerializer(many=True, read_only=True)
+
+
+    def update(self, instance, validated_data):
+        instance.comment = validated_data.get('comment', instance.comment)
+        instance.anonymous = validated_data.get('anonymous',instance.anonymous)
+        instance.ticket_option = validated_data.get('ticket_option', instance.ticket_option)
+        instance.festival = validated_data.get('festival', instance.festival)
+        instance.supporter = validated_data.get('supporter', instance.supporter)
+        instance.save()
+        return instance
+    class Meta:
+        model = Pledge
+        fields = "__all__"
